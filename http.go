98b74a1a1583a9ce99f8f3f36fd519da7b65a381
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-12 08:51:47 +0000

    Add logs endpoint

--- http.go
+++ http.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
+	"regexp"
 )
+
+var regexASCII = regexp.MustCompile(`\[\d+m`)
 
 func httpIndex(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -18,10 +22,29 @@ func httpIndex(w http.ResponseWriter, r *http.Request) {
 
 func httpForbidden(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-	return
 }
 
 func httpBad(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	return
+}
+
+func httpLogs(w http.ResponseWriter, r *http.Request) {
+	response, err := http.Get("http://observer/logs")
+
+	if err != nil || response.StatusCode < 300 {
+		log.Printf("Observer /logs fails: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Issue with fetch the logs: %s", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	logs := regexASCII.ReplaceAllLiteralString(string(data), "")
+	w.Write([]byte(logs))
 }
