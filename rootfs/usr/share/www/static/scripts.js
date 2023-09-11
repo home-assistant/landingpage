b38a90a3d9eeada4cc1cd2b58841ca96316ccbf3
Bram Kragten <mail@bramkragten.nl>
2023-09-11 15:45:17 +0200

    Add error message when logs include "ERROR"

--- rootfs/usr/share/www/static/scripts.js
+++ rootfs/usr/share/www/static/scripts.js
@@ -75,12 +75,23 @@ function fetchLogs() {
     if (res.ok) {
       res.text().then(function (text) {
         var logElement = document.getElementById("log");
-        logElement.innerText = logElement.showFull
-          ? text
-          : ""
-            .replace(/\s[A-Z]+\s\(\w+\)\s\[[\w.]+\]/gi, "")
-            .replace(/\d{2}-\d{2}-\d{2}\s/gi, "")
-            .replace(/\d{2}:\d{2}:\d{2}\s/gi, "");
+        if (text.includes("ERROR")) {
+          document.body.classList.add("error");
+          document.getElementById("show_logs").classList.add("hidden");
+          logElement.showFull = true;
+        }
+        if (!logElement.showFull) {
+          return;
+        }
+        var scrolledDown = logElement.scrollTop + logElement.clientHeight === logElement.scrollHeight;
+        logElement.innerText = text
+          .replace(/\s[A-Z]+\s\(\w+\)\s\[[\w.]+\]/gi, "")
+          .replace(/\d{2}-\d{2}-\d{2}\s/gi, "")
+          .replace(/\d{2}:\d{2}:\d{2}\s/gi, "");
+        if (scrolledDown) {
+          // Scroll content down if it was already scrolled down
+          logElement.scrollTop = logElement.scrollHeight;
+        }
       });
     }
   }, scheduleFetchLogs());
@@ -106,10 +117,11 @@ function toggleLogs(event) {
   logElement.showFull = !logElement.showFull;
   if (logElement.showFull) {
     event.target.innerText = "Hide details";
+    fetchLogs();
   } else {
+    logElement.innerText = "";
     event.target.innerText = "Show details";
   }
-  fetchLogs();
 }
 
 var dialogs = document.querySelectorAll('dialog');
